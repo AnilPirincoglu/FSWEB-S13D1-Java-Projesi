f31a7960cc2e71a28c2f05d4e86ce499634645c7
AnilP. <anl.pirincoglu@gmail.com>
2024-01-08 15:13:59 +0300

    adds shouldWakeUp method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean isWakeUp = shouldWakeUp(true,-1);
+        System.out.println(isWakeUp);
+    }
+
+    public static boolean shouldWakeUp(boolean bark, int hour){
+        if(bark){
+            return (0 <= hour && hour <= 8) || (20 <= hour && hour <= 23);
+        }
+        return false;
     }
 }
