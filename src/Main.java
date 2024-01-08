1a1fffa782df8ff96e67cd4dcd0edc0ce5305b96
AnilP. <anl.pirincoglu@gmail.com>
2024-01-08 15:15:48 +0300

    adds hasTeen method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         boolean isWakeUp = shouldWakeUp(true,-1);
         System.out.println(isWakeUp);
+
+        boolean isTeen = hasTeen(12,19,25);
+        System.out.println(isTeen);
     }
 
     public static boolean shouldWakeUp(boolean bark, int hour){
@@ -9,5 +12,8 @@ public class Main {
             return (0 <= hour && hour <= 8) || (20 <= hour && hour <= 23);
         }
         return false;
+    }
+    public static boolean hasTeen (int param1,int param2, int param3){
+        return ((13<=param1 && param1<=19)||(13<=param2 && param2<=19)||(13<=param3 && param3<=19));
     }
 }
